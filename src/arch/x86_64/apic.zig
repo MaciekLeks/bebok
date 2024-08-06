472ffd664a9e1469d3439e08758f01803fe4e53a
Maciej Leks <maciej.leks@gmail.com>
2024-08-06 13:20:01 +0200

    feat(apic): add support for IA32 APIC base MSR

--- src/arch/x86_64/apic.zig
+++ src/arch/x86_64/apic.zig
@@ -3,19 +3,28 @@ const cpu = @import("cpu.zig");
 
 const log = std.log.scoped(.apic);
 
-fn lapicSupported() bool {
+const ia32_apic_base_msr_addr = 0x1B;
+
+fn isLapicSupported() bool {
     const cpuid = cpu.cpuid(0x1);
     return cpuid.edx & (1 << 9) != 0;
+}
+
+fn enableLapic() void {
+    const lapic_base = cpu.rdmsr(ia32_apic_base_msr_addr);
+    cpu.wrmsr(ia32_apic_base_msr_addr, lapic_base | (1 << 11));
 }
 
 pub fn init() !void {
     log.info("Initializing APIC", .{});
     defer log.info("APIC initialized", .{});
 
-    const lapic_supported = lapicSupported();
+    const lapic_supported = isLapicSupported();
     log.info("Checking if LAPIC is supported: {}", .{lapic_supported});
 
     if (!lapic_supported) {
         return error.LAPICNotSupported;
     }
+
+    enableLapic();
 }
