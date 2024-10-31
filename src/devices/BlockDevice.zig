89d5696cd71f7d622f16a58a2041881490550073
Maciek Leks <maciej.leks@gmail.com>
2024-10-31 13:19:41 +0100

    block_device: read and write inside Streamer

--- src/devices/BlockDevice.zig
+++ src/devices/BlockDevice.zig
@@ -2,6 +2,10 @@ const std = @import("std");
 const NvmeController = @import("deps.zig").NvmeController;
 const Device = @import("Device.zig");
 
+const heap = @import("deps.zig").heap; //TODO: tbd
+
+const log = std.log.scoped(.blockl_device);
+
 const BlockDevice = @This();
 
 alloctr: std.mem.Allocator,
@@ -27,31 +31,57 @@ pub fn deinit(self: *BlockDevice) void {
 
 // TODO: can't use generic Steamer(T), see: https://www.reddit.com/r/Zig/comments/1gcexso/dynamic_interface_with_comptime_vtable_functions/
 pub const Streamer = struct {
+    pub const LbaPos = struct {
+        slba: u64,
+        nlba: u16,
+        slba_offset: u64,
+    };
+
     pub const VTable = struct {
-        read: *const fn (ctx: *anyopaque, allocator: std.mem.Allocator, offset: usize, total: usize) anyerror![]u8,
-        write: *const fn (ctx: *anyopaque, offset: usize, buf: []u8) anyerror!void,
+        //read: *const fn (ctx: *anyopaque, allocator: std.mem.Allocator, offset: usize, total: usize) anyerror![]u8,
+        read: *const fn (ctx: *anyopaque, allocator: std.mem.Allocator, offset: u64, total: u16) anyerror![]u8,
+        //write: *const fn (ctx: *anyopaque, offset: usize, buf: []u8) anyerror!void,
+        write: *const fn (ctx: *anyopaque, allocator: std.mem.Allocator, slba: u64, data: []const u8) anyerror!void,
+        calculate: *const fn (ctx: *anyopaque, offset: usize, total: usize) anyerror!Streamer.LbaPos,
     };
 
     ptr: *anyopaque,
     vtable: VTable,
 
-    pub fn read(self: Streamer, allocator: std.mem.Allocator, offset: usize, total: usize) anyerror![]u8 {
-        return self.vtable.read(self.ptr, allocator, offset, total);
+    pub fn read(self: Streamer, comptime T: type, inalloctr: std.mem.Allocator, allocator: std.mem.Allocator, offset: usize, total: usize) anyerror![]T {
+        const total_bytes = total * @sizeOf(T);
+        const lba = try self.vtable.calculate(self.ptr, offset, total_bytes);
+        log.debug("read(): Calculated LBA: {d}, NLBA: {d}, Offset: {d}", .{ lba.slba, lba.nlba, lba.slba_offset });
+
+        const data = try self.vtable.read(self.ptr, inalloctr, lba.slba, lba.nlba);
+        defer inalloctr.free(data);
+
+        const buf = allocator.alloc(T, total) catch |err| {
+            log.err("read(): Failed to allocate memory for data buffer: {}", .{err});
+            return error.OutOfMemory;
+        };
+
+        @memcpy(buf, std.mem.bytesAsSlice(T, data[lba.slba_offset .. lba.slba_offset + total_bytes]));
+
+        return buf;
     }
 
     pub fn write(self: Streamer, offset: usize, buf: []u8) anyerror!void {
-        return self.vtable.write(self.ptr, offset, buf);
+        _ = offset;
+        _ = buf;
+        _ = self;
+        //return self.vtable.write(self.ptr, offset, buf);
     }
 
-    pub fn readAs(self: Streamer, comptime T: type, allocator: std.mem.Allocator, offset: usize, total: usize) ![]T {
-        const bytes = try self.read(allocator, offset, total * @sizeOf(T));
-        return std.mem.bytesAsSlice(T, bytes);
-    }
+    // pub fn readAs(self: Streamer, comptime T: type, allocator: std.mem.Allocator, offset: usize, total: usize) ![]T {
+    //     const bytes = try self.read(allocator, offset, total * @sizeOf(T));
+    //     return std.mem.bytesAsSlice(T, bytes);
+    // }
 
-    pub fn writeFrom(self: Streamer, comptime T: type, offset: usize, data: []const T) !void {
-        const bytes = std.mem.sliceAsBytes(data);
-        try self.write(offset, bytes);
-    }
+    // pub fn writeFrom(self: Streamer, comptime T: type, offset: usize, data: []const T) !void {
+    //     const bytes = std.mem.sliceAsBytes(data);
+    //     try self.write(offset, bytes);
+    // }
 
     pub fn init(ctx: *anyopaque, vtable: VTable) Streamer {
         return .{
@@ -75,22 +105,24 @@ pub fn Stream(comptime T: type) type {
 
         streamer: Streamer,
         pos: usize, //in bytes
+        alloctr: std.mem.Allocator,
 
         pub fn init(streamer: Streamer) Self {
             return .{
                 .streamer = streamer,
                 .pos = 0,
+                .alloctr = heap.page_allocator, //TODO: make more flexible
             };
         }
 
         pub fn read(self: *Self, allocator: std.mem.Allocator, total: usize) anyerror![]T {
-            const data = try self.streamer.readAs(T, allocator, self.pos, total);
+            const data = try self.streamer.read(T, self.alloctr, allocator, self.pos, total);
             self.pos += total * @sizeOf(T);
             return data;
         }
 
         pub fn write(self: *Self, buf: []T) anyerror!void {
-            try self.streamer.writeAs(self.pos, buf);
+            try self.streamer.write(self.pos, buf);
             self.pos += buf.len * @sizeOf(T);
         }
 
--- src/modules/nvme/NvmeNamespace.zig
+++ src/modules/nvme/NvmeNamespace.zig
@@ -42,18 +42,18 @@ pub fn deinit(self: *NvmeNamespace) void {
 // @return Streamer interface
 pub fn streamer(self: *NvmeNamespace) Streamer {
     const vtable = Streamer.VTable{
-        .read = read,
-        .write = write,
+        .read = readInternal,
+        .write = writeInternal,
+        .calculate = calculateInternal,
     };
     return Streamer.init(self, vtable);
 }
 
-fn calculateLba(offset: usize, total: usize, namespace_info: id.NsInfo) !struct {
-    slba: u64,
-    nlba: u16,
-    slba_offset: u64,
-} {
-    const lbads_bytes = math.pow(u32, 2, namespace_info.lbaf[namespace_info.flbas].lbads);
+/// Calculate the LBA from the offset and total bytes to read/write
+pub fn calculateInternal(ctx: *anyopaque, offset: usize, total: usize) !Streamer.LbaPos {
+    const self: *const NvmeNamespace = @ptrCast(@alignCast(ctx));
+
+    const lbads_bytes = math.pow(u32, 2, self.info.lbaf[self.info.flbas].lbads);
     const slba = offset / lbads_bytes;
     const nlba: u16 = @intCast(try std.math.divCeil(usize, total, lbads_bytes));
     const slba_offset = offset % lbads_bytes;
@@ -67,45 +67,47 @@ fn calculateLba(offset: usize, total: usize, namespace_info: id.NsInfo) !struct 
 /// @param offset : Offset to read from
 /// @param total : Total bytes to read
 /// TODO: if more devices supports LBA than this can be move out to BlockDevice
-pub fn read(ctx: *anyopaque, allocator: std.mem.Allocator, offset: usize, total: usize) anyerror![]u8 {
-    const self: *const NvmeNamespace = @ptrCast(@alignCast(ctx));
-
-    const lba = try calculateLba(offset, total, self.info);
-    log.debug("read(): Calculated LBA: {d}, NLBA: {d}, Offset: {d}", .{ lba.slba, lba.nlba, lba.slba_offset });
-
-    const data = try self.readInternal(u8, lba.slba, lba.nlba);
-    defer self.alloctr.free(data);
-    //defer heap.page_allocator.free(data);
-
-    const buf = allocator.alloc(u8, total) catch |err| {
-        log.err("read(): Failed to allocate memory for data buffer: {}", .{err});
-        return error.OutOfMemory;
-    };
-
-    @memcpy(buf, data[lba.slba_offset .. lba.slba_offset + total]);
-
-    return buf;
-}
+// pub fn read(ctx: *anyopaque, allocator: std.mem.Allocator, offset: usize, total: usize) anyerror![]u8 {
+//     const self: *const NvmeNamespace = @ptrCast(@alignCast(ctx));
+//
+//     const lba = try calculateLba(offset, total, self.info);
+//     log.debug("read(): Calculated LBA: {d}, NLBA: {d}, Offset: {d}", .{ lba.slba, lba.nlba, lba.slba_offset });
+//
+//     const data = try self.readInternal(u8, lba.slba, lba.nlba);
+//     defer self.alloctr.free(data);
+//     //defer heap.page_allocator.free(data);
+//
+//     const buf = allocator.alloc(u8, total) catch |err| {
+//         log.err("read(): Failed to allocate memory for data buffer: {}", .{err});
+//         return error.OutOfMemory;
+//     };
+//
+//     @memcpy(buf, data[lba.slba_offset .. lba.slba_offset + total]);
+//
+//     return buf;
+// }
 
 /// Write to the NVMe namespace
 /// @param ctx : pointer to NvmeNamespace
 /// @param offset : Offset to write to
 /// @param buf : Data to write
 /// TODO: if more devices supports LBA than this can be move out to BlockDevice
-pub fn write(ctx: *anyopaque, offset: usize, buf: []u8) anyerror!void {
-    const self: *const NvmeNamespace = @ptrCast(@alignCast(ctx));
-
-    const lba = try calculateLba(offset, buf.len, self.info);
-    log.debug("write(): Calculated LBA: {d}, NLBA: {d}, Offset: {d}", .{ lba.slba, lba.nlba, lba.slba_offset });
-
-    return error.NotImplemented;
-}
+// pub fn write(ctx: *anyopaque, offset: usize, buf: []u8) anyerror!void {
+//     const self: *const NvmeNamespace = @ptrCast(@alignCast(ctx));
+//
+//     const lba = try calculateLba(offset, buf.len, self.info);
+//     log.debug("write(): Calculated LBA: {d}, NLBA: {d}, Offset: {d}", .{ lba.slba, lba.nlba, lba.slba_offset });
+//
+//     return error.NotImplemented;
+// }
 
 /// Read from the NVMe namespace
 /// @param allocator : User allocator
 /// @param slba : Start Logical Block Address
 /// @param nlb : Number of Logical Blocks
-fn readInternal(self: *const NvmeNamespace, comptime T: type, slba: u64, nlba: u16) ![]T {
+pub fn readInternal(ctx: *anyopaque, allocator: std.mem.Allocator, slba: u64, nlba: u16) ![]u8 {
+    const self: *const NvmeNamespace = @ptrCast(@alignCast(ctx));
+
     log.debug("Namespace {d} info: {}", .{ self.nsid, self.info });
 
     if (slba > self.info.nsze) return e.NvmeError.InvalidLBA;
@@ -122,7 +124,7 @@ fn readInternal(self: *const NvmeNamespace, comptime T: type, slba: u64, nlba: u
     log.debug("Number of pages to allocate: {d} to load: {d} bytes", .{ page_count, total_bytes });
 
     // calculate the physical address of the data buffer
-    const data = self.alloctr.alloc(T, total_bytes / @sizeOf(T)) catch |err| {
+    const data = allocator.alloc(u8, total_bytes) catch |err| {
         //const data = heap.page_allocator.alloc(T, total_bytes / @sizeOf(T)) catch |err| {
         log.err("Failed to allocate memory for data buffer: {}", .{err});
         return error.OutOfMemory;
@@ -151,7 +153,7 @@ fn readInternal(self: *const NvmeNamespace, comptime T: type, slba: u64, nlba: u
                 return error.NotImplemented;
             }
 
-            prp_list = self.alloctr.alloc(usize, entry_count) catch |err| {
+            prp_list = allocator.alloc(usize, entry_count) catch |err| {
                 log.err("Failed to allocate memory for PRP list: {}", .{err});
                 return error.OutOfMemory;
             };
@@ -168,15 +170,15 @@ fn readInternal(self: *const NvmeNamespace, comptime T: type, slba: u64, nlba: u
             break :blk try paging.physFromPtr(&prp_list.?[0]);
         },
     };
-    defer if (prp_list) |pl| self.alloctr.free(pl);
+    defer if (prp_list) |pl| allocator.free(pl);
     log.debug("PRP1: 0x{x}, PRP2: 0x{x}", .{ prp1_phys, prp2_phys });
 
     // allotate memory for Metadata buffer
-    const metadata = self.alloctr.alloc(u8, nlba * flbaf.ms) catch |err| {
+    const metadata = allocator.alloc(u8, nlba * flbaf.ms) catch |err| {
         log.err("Failed to allocate memory for metadata buffer: {}", .{err});
         return error.OutOfMemory;
     };
-    defer self.alloctr.free(metadata);
+    defer allocator.free(metadata);
     @memset(metadata, 0);
 
     const mptr_phys = paging.physFromPtr(metadata.ptr) catch |err| {
@@ -235,11 +237,8 @@ fn readInternal(self: *const NvmeNamespace, comptime T: type, slba: u64, nlba: u
 /// @param nsid : Namespace ID
 /// @param slba : Start Logical Block Address
 /// @param data : Data to write
-fn writeInternal(self: *const NvmeNamespace, T: type, slba: u64, data: []const T) !void {
-    // const ns: id.NsInfo = self.ns_info_map.get(nsid) orelse {
-    //     log.err("Namespace {d} not found", .{nsid});
-    //     return e.NvmeError.InvalidNsid;
-    // };
+pub fn writeInternal(ctx: *anyopaque, allocator: std.mem.Allocator, slba: u64, data: []const u8) !void {
+    const self: *const NvmeNamespace = @ptrCast(@alignCast(ctx));
 
     log.debug("Namespace {d} info: {}", .{ self.nsid, self.info });
 
@@ -252,7 +251,7 @@ fn writeInternal(self: *const NvmeNamespace, T: type, slba: u64, data: []const T
     log.debug("LBA Data Size: {d} bytes", .{lbads_bytes});
 
     // nlba = number of logical blocks to write
-    const data_total_bytes = data.len * @sizeOf(T);
+    const data_total_bytes = data.len;
     const nlba: u16 = @intCast(try std.math.divCeil(usize, data_total_bytes, lbads_bytes));
 
     //calculate number of pages to allocate
@@ -282,7 +281,7 @@ fn writeInternal(self: *const NvmeNamespace, T: type, slba: u64, data: []const T
                 return error.NotImplemented;
             }
 
-            prp_list = self.alloctr.alloc(usize, entry_count) catch |err| {
+            prp_list = allocator.alloc(usize, entry_count) catch |err| {
                 log.err("Failed to allocate memory for PRP list: {}", .{err});
                 return error.OutOfMemory;
             };
@@ -299,15 +298,15 @@ fn writeInternal(self: *const NvmeNamespace, T: type, slba: u64, data: []const T
             break :blk try paging.physFromPtr(&prp_list.?[0]);
         },
     };
-    defer if (prp_list) |pl| self.alloctr.free(pl);
+    defer if (prp_list) |pl| allocator.free(pl);
     log.debug("PRP1: 0x{x}, PRP2: 0x{x}", .{ prp1_phys, prp2_phys });
 
     // allotate memory for Metadata buffer
-    const metadata = self.alloctr.alloc(u8, nlba * flbaf.ms) catch |err| {
+    const metadata = allocator.alloc(u8, nlba * flbaf.ms) catch |err| {
         log.err("Failed to allocate memory for metadata buffer: {}", .{err});
         return error.OutOfMemory;
     };
-    defer self.alloctr.free(metadata);
+    defer allocator.free(metadata);
     @memset(metadata, 0);
 
     const mptr_phys = paging.physFromPtr(metadata.ptr) catch |err| {
