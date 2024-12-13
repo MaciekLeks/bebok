c10403ddf4126fd24790c1cf5a06fcaf734d9ffb
Maciek Leks <maciej.leks@gmail.com>
2024-12-13 08:54:52 +0100

    refactor(Device): implement interface pattern with the use of @fieldParentPtr for Device /4

--- src/devices/PhysDevice.zig
+++ src/devices/PhysDevice.zig
@@ -16,18 +16,7 @@ pub const VTable = struct {
     //add example function as. someFn(physdev: *PhysDevice ) and then implement fn(*PhysDevice) in the implementator
 };
 
-// pub fn probe(self: Driver, probe_ctx: *const anyopaque) bool {
-//     return @call(.auto, self.vtable.probe, .{ self.ptr, probe_ctx });
-// }
-//
-// pub fn setup(self: Driver, setup_ctx: *const anyopaque) anyerror![]*Device {
-//     return @call(.auto, self.vtable.setup, .{ self.ptr, setup_ctx });
-// }
-
-// pub fn deinit(self: Device) void {
-//     return @call(.auto, self.vtable.deinit, .{self.ptr});
-// }
-
+// helper function to get PhysDevice from Device
 pub fn fromDevice(dev: *Device) *PhysDevice {
     return @fieldParentPtr("device", dev);
 }
