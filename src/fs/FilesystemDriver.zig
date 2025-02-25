b0ca271c5fc719065ae6620c220d155f95c03d05
Maciek Leks <maciej.leks@gmail.com>
2025-02-25 14:25:23 +0100

    refactor(fs): improve filesystem driver initialization with type-safe vtable creation

--- src/fs/FilesystemDriver.zig
+++ src/fs/FilesystemDriver.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const iface = @import("lang").iface;
 
 const BlockDevice = @import("devices").BlockDevice;
 const Partition = @import("devices").Partition;
@@ -14,48 +15,20 @@ ctx: *anyopaque,
 vtable: *const VTable,
 
 pub const VTable = struct {
-    resolve: *const fn (ctx: *anyopaque, allocator: std.mem.Allocator, partition: *Partition) anyerror!?Filesystem,
-    destroy: *const fn (ctx: *anyopaque) void,
+    resolve: iface.Fn(.{ std.mem.Allocator, *Partition }, anyerror!?Filesystem),
+    destroy: iface.Fn(.{}, void),
 };
-
-/// VTable container
-fn VTableContainer(comptime T: type) type {
-    return struct {
-        const Self = @This();
-
-        // we need vtable address
-        pub const vtable = VTable{
-            .resolve = struct {
-                fn resolveInt(ctx: *anyopaque, allocator: std.mem.Allocator, partition: *Partition) anyerror!?Filesystem {
-                    const self: T = @ptrCast(@alignCast(ctx));
-                    return self.resolve(allocator, partition);
-                }
-            }.resolveInt,
-            .destroy = struct {
-                fn destroyInt(ctx: *anyopaque) void {
-                    const self: T = @ptrCast(@alignCast(ctx));
-                    return self.destroy();
-                }
-            }.destroyInt,
-        };
-    };
-}
-
 pub fn init(ctx: anytype) FilesystemDriver {
-    const T = @TypeOf(ctx);
-    comptime if (@typeInfo(T) != .pointer) @compileError("FilesystemDriver.init() requires a pointer to the struct");
-    const VT = VTableContainer(@TypeOf(ctx));
-
     return .{
         .ctx = ctx,
-        .vtable = &VT.vtable,
+        .vtable = iface.gen(@TypeOf(ctx), VTable),
     };
 }
 
 pub fn deinit(self: *const FilesystemDriver) void {
-    return self.vtable.destroy(self.ctx);
+    return self.vtable.destroy(self.ctx, .{});
 }
 
 pub fn resolve(self: *const FilesystemDriver, allocator: std.mem.Allocator, partition: *Partition) anyerror!?Filesystem {
-    return self.vtable.resolve(self.ctx, allocator, partition);
+    return self.vtable.resolve(self.ctx, .{ allocator, partition });
 }
